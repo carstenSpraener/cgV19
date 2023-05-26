d71f42311c69b74ae55e7b3fd4f80f93a462e60c
Carsten Spraener <carsten@spraener.de>
2023-05-26 16:40:26 -0400

    Tests greenification after merge

---
+++ core/cgV19-MetaCartridge/src/main/resources/meta/CartridgeServiceLocaterTemplate.groovy
@@ -0,0 +1,15 @@
+// THIS FILE IS GENERATED AS LONG AS THIS LINE EXISTS
+import de.spraener.nxtgen.ProtectionStrategieDefaultImpl
+import de.spraener.nxtgen.oom.model.MClass
+import de.spraener.nxtgen.oom.model.OOModel
+
+MClass mClass = this.getProperty("modelElement");
+OOModel model = mClass.getModel();
+
+
+"""//${ProtectionStrategieDefaultImpl.GENERATED_LINE}
+package ${mClass.getPackage().getFQName()};
+
+public class ${mClass.getName()} {
+}
+"""
