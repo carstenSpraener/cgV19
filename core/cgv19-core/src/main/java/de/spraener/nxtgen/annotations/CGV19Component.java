edbc2139880ecf39733659b867adba3b202c6358
Carsten Spraener <carsten@spraener.de>
2023-06-23 11:27:27 -0400

    Javadoc for the annotations

--- core/cgv19-core/src/main/java/de/spraener/nxtgen/annotations/CGV19Component.java
+++ core/cgv19-core/src/main/java/de/spraener/nxtgen/annotations/CGV19Component.java
@@ -5,6 +5,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * A class annotated with this annotation can hold several methods
+ * which are annotated with CGV19Generator or CGV19Transformation,
+ * and each such method will be treated as a Generator or Transformation.
+ *
+ * This makes implementation for small cartridges easier and different
+ * aspects can be grouped together in one class.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface CGV19Component {
--- core/cgv19-core/src/main/java/de/spraener/nxtgen/annotations/CGV19Transformation.java
+++ core/cgv19-core/src/main/java/de/spraener/nxtgen/annotations/CGV19Transformation.java
@@ -6,7 +6,15 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
+/**
+ * This annotation marks the annotated target as a transformation for cgV19.
+ * It can be applied to a Class which than has to be an implementation of
+ * Transformation or a method inside a @CGV19Component. The method has to
+ * take a ModelElement parameter.
+ *
+ * @see de.spraener.nxtgen.Transformation
+ * @see CGV19Component
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
 public @interface CGV19Transformation {
