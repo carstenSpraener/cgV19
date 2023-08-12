35fd587ae54244775515edfb132c23e5f80cc07c
Carsten Spraener <carsten@spraener.de>
2023-08-12 11:33:55 -0400

    Again chicken/egg problem with self generating metacartridge

---
+++ core/cgv19-metacartridge/src/main/java-gen/de/spraener/nxtgen/cartridge/meta/StereotypeDescriptorJsonGenerator.java
@@ -0,0 +1,44 @@
+// THIS FILE IS GENERATED AS LONG AS THIS LINE EXISTS
+package de.spraener.nxtgen.cartridge.meta;
+
+import java.util.function.Consumer;
+
+import de.spraener.nxtgen.CodeBlock;
+import de.spraener.nxtgen.CodeGenerator;
+import de.spraener.nxtgen.GroovyCodeBlockImpl;
+import de.spraener.nxtgen.java.JavaCodeBlock;
+import de.spraener.nxtgen.model.ModelElement;
+import de.spraener.nxtgen.oom.model.*;
+import de.spraener.nxtgen.annotations.*;
+
+@CGV19Generator(
+        requiredStereotype = "StereotypeDescriptor",
+        operatesOn = MClass.class,
+        outputType = OutputType.OTHER,
+        outputTo = OutputTo.OTHER,
+        templateName = "/meta/StereotypeDescriptorJsonTemplate.groovy",
+        implementationKind = ImplementationKind.GROOVY_TEMPLATE
+)
+public class StereotypeDescriptorJsonGenerator implements CodeGenerator {
+    private Consumer<CodeBlock>[] codeBlockModifiers;
+
+    public StereotypeDescriptorJsonGenerator(Consumer<CodeBlock>... codeBlockModifiers) {
+        this.codeBlockModifiers = codeBlockModifiers;
+    }
+
+    @Override
+    public CodeBlock resolve(ModelElement element, String templateName) {
+        GroovyCodeBlockImpl gcb = new GroovyCodeBlockImpl("", element, "/meta/StereotypeDescriptorJsonTemplate.groovy");
+        gcb.setToFileStrategy(new de.spraener.nxtgen.filestrategies.GeneralFileStrategy(".", "", ""));
+
+        if( codeBlockModifiers!=null ) {
+            for (Consumer<CodeBlock> codeBlockModifier: this.codeBlockModifiers) {
+                codeBlockModifier.accept(gcb);
+            }
+        }
+
+        return gcb;
+
+    }
+}
+
---
+++ core/cgv19-metacartridge/src/main/java-gen/de/spraener/nxtgen/cartridge/meta/StereotypeEnumToDescriptorTransformationBase.java
@@ -0,0 +1,28 @@
+///THIS FILE IS GENERATED AS LONG AS THIS LINE EXISTS
+package de.spraener.nxtgen.cartridge.meta;
+
+import de.spraener.nxtgen.model.ModelElement;
+import de.spraener.nxtgen.oom.model.*;
+import de.spraener.nxtgen.oom.StereotypeHelper;
+import de.spraener.nxtgen.oom.model.*;
+import de.spraener.nxtgen.annotations.*;
+
+@CGV19Transformation(
+        requiredStereotype = "StereotypeEnum",
+        operatesOn = MClass.class
+)
+public abstract class StereotypeEnumToDescriptorTransformationBase implements de.spraener.nxtgen.Transformation {
+
+    @Override
+    public void doTransformation(ModelElement element) {
+        if( !(element instanceof MClass) ) {
+            return;
+        }
+        if( !StereotypeHelper.hasStereotype(element, "StereotypeEnum")) {
+            return;
+        }
+        doTransformationIntern((MClass)element);
+    }
+
+    public abstract void doTransformationIntern(MClass modelElement);
+}
