c546c873636a2237b406bb6872e932375e3b1c3c
Carsten Spraener <carsten@spraener.de>
2024-09-09 07:32:45 +0200

    Write files as UTF-8

--- core/cgv19-core/src/main/java/de/spraener/nxtgen/SimpleFileWriterCodeBlock.java
+++ core/cgv19-core/src/main/java/de/spraener/nxtgen/SimpleFileWriterCodeBlock.java
@@ -4,6 +4,7 @@ import org.apache.commons.io.IOUtils;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class SimpleFileWriterCodeBlock extends SimpleStringCodeBlock {
     private String filePath;
@@ -15,8 +16,8 @@ public class SimpleFileWriterCodeBlock extends SimpleStringCodeBlock {
 
     @Override
     public void writeOutput(String workingDir) {
-        try( FileWriter fw = new FileWriter(workingDir+"/"+this.filePath); ) {
-            IOUtils.write(this.toCode(), fw);
+        try( FileWriter fw = new FileWriter(workingDir+"/"+this.filePath, StandardCharsets.UTF_8); ) {
+            IOUtils.write(this.toCode().getBytes(StandardCharsets.UTF_8), fw, StandardCharsets.UTF_8);
         } catch( IOException xc ) {
             throw new RuntimeException(xc);
         }
